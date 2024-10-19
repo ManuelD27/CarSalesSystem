33f6467166128ec74489b071192ab0506635838f
ManuelD27 <manuelfduffis@gmail.com>
2024-10-19 11:13:44 +0100

    Add get_input_with_prompt function for cleaner input handling

--- CarSales/main.c
+++ CarSales/main.c
@@ -1,44 +1,47 @@
 #include <stdio.h>
 
-int main(){
-    
+// Function to get input with a prompt
+void get_input_with_prompt(const char *prompt, char *input)
+{
+    printf("%s", prompt);  // Print the prompt
+    scanf("%s", input);    // Store the user's input in the input variable
+}
+
+int main()
+{
     /* Constants */
-#define CAR_PRICE 20500.0f
-#define DISCOUNT_MULTIBUY_PERCENTAGE 0.20f
-#define DISCOUNT_MULTIBUY_AMOUNT 3
-#define DISCOUNT_MEMBER_PERCENTAGE 0.25f
+    #define CAR_PRICE 20500.0f
+    #define DISCOUNT_MULTIBUY_PERCENTAGE 0.20f
+    #define DISCOUNT_MULTIBUY_AMOUNT 3
+    #define DISCOUNT_MEMBER_PERCENTAGE 0.25f
 
     /* Variables */
     unsigned short carsAvailable = 10, carsNeeded = 0, giveDiscount = 0;
     float totalPrice = 0.0f, discountValue;
     char isMemberOfCarClub;
-    char firstName[20], lastName[20], userChoice;
+    char firstName[20];  // Array to store the first name
+    char lastName[20];   // Array to store the last name
     int userAge;
 
-    printf("Enter your first name: "); //This asks user for name and age//
-    
-    scanf("%s", firstName);
-    
-    printf("Enter your last name: ");
-    scanf("%s", lastName);
-    
+    // Use the get_input_with_prompt function to gather input
+    get_input_with_prompt("Enter your first name: ", firstName);
+    get_input_with_prompt("Enter your last name: ", lastName);
+
     printf("Enter your age. Age: ");
     scanf("%d", &userAge);
     
-    printf("Hello %s, Welcome to the Car Sales office!\n\n", firstName);
-    
-    printf("Menu:\n");
+    printf("\nMenu:\n");
     printf("a. Buy Cars\n");
     printf("b. View Sales Stats\n");
     printf("x. Exit\n\n");
-    
-    
+
+    /* Greeting */
+    printf("Hello %s %s, Welcome to the Car Sales office!\n\n", firstName, lastName);
 
     /* Stock info */
     if (carsAvailable > 0)
     {
-        printf("We have %hd cars available. Each car costs %.2f.\n\n",
-               carsAvailable, CAR_PRICE);
+        printf("We have %hd cars available. Each car costs %.2f.\n\n", carsAvailable, CAR_PRICE);
     }
     else
     {
@@ -65,15 +68,14 @@ int main(){
     printf("\nAre you a member of the Car Club? Answer (Y/N) = ");
     scanf("\n%c", &isMemberOfCarClub);
 
-    // check if member discount needs to be applied
-    //        first because % is greater
+    // Check if member discount needs to be applied
     if (isMemberOfCarClub == 'Y')
     {
         giveDiscount = 1;
         totalPrice *= (1 - DISCOUNT_MEMBER_PERCENTAGE);
         discountValue = DISCOUNT_MEMBER_PERCENTAGE;
     }
-        // otherwise, check multibuy discount
+    // Otherwise, check multibuy discount
     else if (carsNeeded >= DISCOUNT_MULTIBUY_AMOUNT)
     {
         giveDiscount = 1;
@@ -85,9 +87,7 @@ int main(){
     switch (giveDiscount)
     {
         case 1:
-            // convert from 0.2 format to 20% format
-            discountValue *= 100;
-            // hide decimals, not needed
+            discountValue *= 100; // Convert from 0.2 format to 20% format
             printf("\nDiscount of %.0f%% applied!\n", discountValue);
             break;
         case 0:
@@ -97,12 +97,10 @@ int main(){
 
     /* Present final outcome */
     printf("\nThank you for your custom.\n");
-    printf("\nYou have bought %hd cars. Total price to pay is %.2f.",
-           carsNeeded, totalPrice);
-
+    printf("\nYou have bought %hd cars. Total price to pay is %.2f.", carsNeeded, totalPrice);
     printf("\n\nThere are %hd cars remaining.", carsAvailable);
 
-    printf("\n\nThank you. Have a good day!");
+    printf("\n\nThank you. Have a good day!\n");
 
     return 0;
 }
